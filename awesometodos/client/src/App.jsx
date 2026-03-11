0b6133f2601f51cb03468fe51d43492bf60e07c4
SHUVO DEBNATH <cfshuvo4@gmail.com>
2026-03-12 04:03:24 +0600

    Connect Back end to Front end

--- awesometodos/client/src/App.jsx
+++ awesometodos/client/src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("/api/todos");
+      const todos = await res.json();
+
+      setMessage(todos.mssg);
+    }
+    getTodos();
+  }, [])
+
+
   return (
     <main className="container">
       <h1>Website BUIT Todos</h1>
+      {message && <p>{message}</p>}
     </main>
   );
 }
