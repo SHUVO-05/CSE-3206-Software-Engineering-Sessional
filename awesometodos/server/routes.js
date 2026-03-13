cc10c2c97c29ec9617ffd05b39ea3d715973000a
SHUVO DEBNATH <cfshuvo4@gmail.com>
2026-03-14 03:37:15 +0600

    Activity Update

--- awesometodos/server/routes.js
+++ awesometodos/server/routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
 const router = express.Router();
+const { getConnectedClient } = require("./database");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("todosdb").collection("todos");
+    return collection;
+}
 
 //GET /todos
-router.get("/todos", (req, res) => {
-    res.status(200).json({ mssg: "GET REQUEST TO /api/todos"});
-});
+router.get("/todos", async(req, res) => {
+    const collection = getCollection();
+    const todos = await collection.find({}).toArray();
+
+    res.status(200).json(todos);
+});    
 
 //POST /todos
 router.post("/todos", (req,res) => {
